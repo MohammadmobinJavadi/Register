62e4101e3af718253ead862cc24a06577048b7c5
MohammadmobinJavadi <112621962+MohammadmobinJavadi@users.noreply.github.com>
2022-09-01 16:38:33 +0430

    Add files via upload

---
+++ Human.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sabt_name
+{
+    class Human
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string family { get; set; }
+        public byte age { get; set; }
+    }
+
+}
