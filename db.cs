7ee19f75604eeac7c3d34d9052fef2d59969eb81
MohammadmobinJavadi <112621962+MohammadmobinJavadi@users.noreply.github.com>
2022-09-01 16:38:10 +0430

    Add files via upload

---
+++ db.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace sabt_name
+{
+    class db : DbContext
+    {
+        public db():base("sabt name")
+        {
+
+        }
+        public DbSet<Human> Humans { set; get; }
+        
+      
+    }
+}
